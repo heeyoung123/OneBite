aff185ceb6636db87910d92662ad79dcf2d3449a
heeyoung123 <heeyoung321@naver.com>
2024-07-02 23:57:41 +0900

    가운데정렬지움

--- section112/section112/src/components/Editor.jsx
+++ section112/section112/src/components/Editor.jsx
@@ -1,23 +1,49 @@
 import "./Editor.scss";
 import EmotionItem from "./EmotionItem";
 
+const emotionList = [
+  {
+    emotionId: 1,
+    emotionName: "완전 좋음",
+  },
+  {
+    emotionId: 2,
+    emotionName: "좋음",
+  },
+  {
+    emotionId: 3,
+    emotionName: "그럭저럭",
+  },
+  {
+    emotionId: 4,
+    emotionName: "나쁨",
+  },
+  {
+    emotionId: 5,
+    emotionName: "끔찍함",
+  },
+];
+
 const Editor = () => {
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
         <input type="date" />
-        <div>
-          <EmotionItem emotionId={1} emotionName={"완전좋음"} />
-          <EmotionItem />
-          <EmotionItem />
-          <EmotionItem />
-          <EmotionItem />
+      </section>
+
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem key={item.emotionId} {...item} />
+          ))}
         </div>
       </section>
-      <h4>오늘의 감정</h4>
-      <section className="emotion_section"></section>
-      <section className="content_section"></section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea placeholder="오늘은 어땠나요?" />
+      </section>
       <section className="button_section"></section>
     </div>
   );
--- section112/section112/src/components/EmotionItem.jsx
+++ section112/section112/src/components/EmotionItem.jsx
@@ -1,12 +1,12 @@
 import "./EmotionItem.scss";
 import { getEmotionImage } from "../util/get-emotion-image";
+
 const EmotionItem = ({ emotionId, emotionName }) => {
   return (
-    <div>
-      <img src={getEmotionImage(emotionId)} />
-      <div>{emotionName}</div>
+    <div className="EmotionItem">
+      <img className="emotion_img" src={getEmotionImage(emotionId)} />
+      <div className="emotion_name">{emotionName}</div>
     </div>
   );
 };
 export default EmotionItem;
-//emotionid와 emotionName은언제쓰지?여기서 쓰는거일텐데..
