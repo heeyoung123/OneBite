07044ca9e74a48eeffb91f62ea96733559b3c9ea
heeyoung123 <heeyoung321@naver.com>
2024-07-04 17:50:16 +0900

    new 기능

--- section112/section112/src/components/Editor.jsx
+++ section112/section112/src/components/Editor.jsx
@@ -1,7 +1,7 @@
 import "./Editor.scss";
 import EmotionItem from "./EmotionItem";
 import Button from "./button";
-
+import { useState } from "react";
 const emotionList = [
   {
     emotionId: 1,
@@ -24,15 +24,50 @@ const emotionList = [
     emotionName: "끔찍함",
   },
 ];
+const getStringDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = (targetDate.getMonth() + 1).toString().padStart(2, "0");
+  let date = targetDate.getDate().toString().padStart(2, "0");
 
+  return `${year}-${month}-${date}`;
+};
 const Editor = () => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
+  const onChangeInput = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
   const emotionId = 1;
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        {/* input안에 있는 createdDate인 new Date()가 getStringDate메소드에 의해 문자열로 변환됨 */}
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringDate(input.createdDate)}
+          type="date"
+        />
       </section>
 
       <section className="emotion_section">
@@ -40,6 +75,7 @@ const Editor = () => {
         <div className="emotion_list_wrapper">
           {emotionList.map((item) => (
             <EmotionItem
+              value={input.emotionId}
               key={item.emotionId}
               {...item}
               isSelected={item.emotionId === emotionId}
@@ -49,7 +85,7 @@ const Editor = () => {
       </section>
       <section className="content_section">
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea value={input.content} placeholder="오늘은 어땠나요?" />
       </section>
       <section className="button_section">
         <Button text={"취소"} />
