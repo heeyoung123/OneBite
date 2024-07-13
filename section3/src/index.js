7b0f6c76678cdd178c4ad87e1f9ecab871786fce
heeyoung123 <heeyoung321@naver.com>
2024-07-13 17:04:47 +0900

    112

--- section3/src/index.js
+++
@@ -1,8 +0,0 @@
-import mul, {add, sub} from "./math.js";
-import randomColor from "randomcolor";
-
-const color=randomColor();
-console.log(color);s
-// console.log(add(2,4));
-// console.log(sub(6,2));
-// console.log(mul(3,4));
